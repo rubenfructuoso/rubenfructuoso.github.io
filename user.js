df0be33a8d712abaf2eb92d99025dd08af55de97
Rubén Fructuoso <ruben.fructuoso@gmail.com>
2018-06-28 15:08:10 +0200

    on fehler komma statt punkt korrigiert

--- user.js
+++ user.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
   alert('gopfetami!');
-  $('body').on('click'.function(){
+  $('body').on('click', function(){
     $(this).css('background','red');
     $('h1').css('color','blue');
   }
