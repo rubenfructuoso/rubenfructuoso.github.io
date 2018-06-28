f13a29798cfc7b9694a4406188f62632ea03d89d
Rubén Fructuoso <ruben.fructuoso@gmail.com>
2018-06-28 13:10:06 +0200

    back to click

--- user.js
+++ user.js
@@ -1,4 +1,14 @@
+<<<<<<< HEAD
 $('body').toggle(
   function(){$('body').css('background','red');},
   function(){$('body').css('background','white');},
+=======
+$(document).ready(function(){
+  alert('gopfetami!');
+  $('body').click(function(){
+    $(this).css('background','red');
+  }
+  );
+}
+>>>>>>> parent of cb09d0a... finally it funcitons well oder
 );
